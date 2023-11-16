98e7a76355685b0777e06620d73d035682dfba59
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-11-16 15:14:52 +0530

    mentor query added

--- controllers/mentorController.js
+++ controllers/mentorController.js
@@ -1,14 +1,42 @@
 import pool from "./../utils/postgres-connection";
 
-const getAllMentee = async (req, res) => {
-  const result = await pool.query(
-    `SELECT id FROM public.data_node where type=$1 and name ILIKE $2`,
-    ["Competency", "%" + courseSearchRequestData.request.query + "%"]
+const getAllMenteeForMentor = async (req, res) => {
+  const mentorId = req.query.mentorId;
+  const getAllMenteeResponse = await pool.query(
+    `SELECT mentor_mentee.mentor_mentee_id,mentor_mentee.mentor_id,mentor_mentee.mentee_id,mentor_mentee_observation.observation_id,mentor_mentee_observation.status FROM mentor_mentee JOIN mentor_mentee_observation ON mentor_mentee.mentor_mentee_id = mentor_mentee_observation.mentor_mentee_realtion_id WHERE mentor_id=$1`,
+    [mentorId]
   );
   res.status(200).json({
-    message: "Mentee successfully mapped",
-    data: result,
+    message: "SUCCESS",
+    data: getAllMenteeResponse,
   });
 };
+const getObservationForMentee = async (req, res) => {
+  const { mentorId, menteeId } = req.query;
 
-module.exports = { getAllMentee };
+  const getObservationForMenteeResponse = await pool.query(
+    `SELECT mentor_mentee.mentor_mentee_id,mentor_mentee.mentor_id,mentor_mentee.mentee_id,mentor_mentee_observation.observation_id,mentor_mentee_observation.status FROM mentor_mentee JOIN mentor_mentee_observation ON mentor_mentee.mentor_mentee_id = mentor_mentee_observation.mentor_mentee_realtion_id WHERE mentor_id=$1 AND mentee_id=$2;`,
+    [mentorId, menteeId]
+  );
+  res.status(200).json({
+    message: "SUCCESS",
+    data: getObservationForMenteeResponse,
+  });
+};
+const getAllMentorForMentee = async (req, res) => {
+  const { menteeId } = req.query;
+
+  const getObservationForMenteeResponse = await pool.query(
+    `SELECT DISTINCT mentor_mentee.mentor_id FROM mentor_mentee JOIN mentor_mentee_observation ON mentor_mentee.mentor_mentee_id = mentor_mentee_observation.mentor_mentee_realtion_id WHERE mentee_id=$1;`,
+    [menteeId]
+  );
+  res.status(200).json({
+    message: "SUCCESS",
+    data: getObservationForMenteeResponse,
+  });
+};
+module.exports = {
+  getAllMenteeForMentor,
+  getObservationForMentee,
+  getAllMentorForMentee,
+};
--- routes/mentorRoute.js
+++ routes/mentorRoute.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const relationshipController = require("./../controllers/relationshipController");
 const mentorRouter = express.Router();
-mentorRouter.post("/getAllMentee", mentorRouter.getAllMentee);
+mentorRouter.get("/getAllMenteeForMentor", mentorRouter.getAllMenteeForMentor);
+mentorRouter.get("/getAllMentorForMentee", mentorRouter.getAllMentorForMentee);
+
+mentorRouter.get(
+  "/getObservationForMentee",
+  mentorRouter.getObservationForMentee
+);
 module.exports = mentorRouter;
