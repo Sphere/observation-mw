9665e43772c082d9e49eb4a20f4f4e47297bed6b
sumit.bajaj-tarento <sumit.bajaj@tarento.com>
2023-12-12 15:32:34 +0530

    body-parser added

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import express from "express";
 import { router } from "./routes/index";
 import { logger } from "./utils/logger";
 import { authCheck } from "../src/utils/authCheck";
+import { bodyParser } from "body-parser"
 
 const app = express();
+
 app.use((req, _res, next) => {
   logger.info(`Requested Route: ${req.method} ${req.url}`);
   next();
 });
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 app.use(authCheck);
 app.use("/v1", router);
 app.listen(3009, () => {
