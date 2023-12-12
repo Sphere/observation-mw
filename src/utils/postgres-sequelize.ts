236bcd7402d8afbe7238603dc49bf2f0099119e6
sumit.bajaj-tarento <sumit.bajaj@tarento.com>
2023-12-12 10:43:00 +0530

    sequelize connection added

---
+++ src/utils/postgres-sequelize.ts
@@ -0,0 +1,24 @@
+import { logger } from "./logger";
+
+const { Sequelize } = require("sequelize");
+const postgresConnectionDetails = {
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER
+}
+const sequelize = new Sequelize(postgresConnectionDetails.database, postgresConnectionDetails.user, postgresConnectionDetails.password, {
+    host: postgresConnectionDetails.host,
+    port: postgresConnectionDetails.port,
+    dialect: 'postgres'
+})
+const checkConnectionDetails = async () => {
+    try {
+        await sequelize.authenticate();
+        logger.info('Connection has been established successfully.');
+    } catch (error) {
+        logger.info('Unable to connect to the database:', error);
+    }
+}
+checkConnectionDetails();
