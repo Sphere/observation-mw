bae2c7fb20137e227dafb960efbf163365336b24
sumit.bajaj-tarento <sumit.bajaj@tarento.com>
2023-12-14 11:50:12 +0530

    sequelize added in mentor route

--- src/controllers/mentorController.ts
+++ src/controllers/mentorController.ts
@@ -1,26 +1,43 @@
-import { pool } from "./../utils/postgres-connection";
-
+import { MentoringRelationship } from "../models/mentoringRelationshipModel"
+import { MentoringObservation } from "../models/mentoringObservationModel"
 const getAllMenteeForMentor = async (req, res) => {
-  const mentorId = req.query.mentorId;
-  const getAllMenteeResponse = await pool.query(
-    `SELECT mentoring_relationship.mentoring_relationship_id,mentoring_relationship.mentor_id,mentoring_relationship.mentee_id,mentoring_observation.observation_id,mentoring_observation.observation_status FROM mentoring_relationship JOIN mentoring_observation ON mentoring_relationship.mentoring_relationship_id = mentoring_observation.mentoring_relationship_id WHERE mentor_id=$1`,
-    [mentorId]
-  );
-  res.status(200).json({
-    message: "SUCCESS",
-    data: getAllMenteeResponse.rows,
+  const { mentorId } = req.query;
+
+
+  MentoringRelationship.findAll({
+    attributes: ['mentoring_relationship_id', 'mentor_id', 'mentee_id', 'mentor_name', 'mentee_name'],
+    include: [{
+      model: MentoringObservation,
+      attributes: ['uuid_id', 'type', 'observation_id', 'observation_status', 'submission_status', 'additional_data', 'createdAt', 'updatedAt'],
+      where: { mentor_id: mentorId }, // Add the condition to filter by mentor_id
+    }],
+  }).then(result => {
+    res.status(200).json({
+      message: "SUCCESS",
+      data: result,
+    });
+  }).catch(error => {
+    console.error('Error:', error);
   });
+
 };
 const getObservationForMentee = async (req, res) => {
   const { menteeId } = req.query;
 
-  const getObservationForMenteeResponse = await pool.query(
-    `SELECT mentoring_relationship.mentoring_relationship_id,mentoring_relationship.mentor_id,mentoring_relationship.mentee_id,mentoring_observation.observation_id,mentoring_observation.observation_status FROM mentoring_relationship JOIN mentoring_observation ON mentoring_relationship.mentoring_relationship_id = mentoring_observation.mentoring_relationship_id WHERE mentee_id=$1;`,
-    [menteeId]
-  );
-  res.status(200).json({
-    message: "SUCCESS",
-    data: getObservationForMenteeResponse.rows,
+  MentoringRelationship.findAll({
+    attributes: ['mentoring_relationship_id', 'mentor_id', 'mentee_id', 'mentor_name', 'mentee_name'],
+    include: [{
+      model: MentoringObservation,
+      attributes: ['uuid_id', 'type', 'observation_id', 'observation_status', 'submission_status', 'additional_data', 'createdAt', 'updatedAt'],
+      where: { mentee_id: menteeId }, // Add the condition to filter by mentee_id
+    }],
+  }).then(result => {
+    res.status(200).json({
+      message: "SUCCESS",
+      data: result,
+    });
+  }).catch(error => {
+    console.error('Error:', error);
   });
 };
 
