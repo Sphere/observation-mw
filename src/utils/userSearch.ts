8fc1e794b15cdc39fa36edf1551c412d71833c32
sumit.bajaj-tarento <sumit.bajaj@tarento.com>
2023-12-15 15:33:23 +0530

    logger added

--- src/utils/userSearch.ts
+++ src/utils/userSearch.ts
@@ -41,7 +41,7 @@ export const userContactInfo = async (userId) => {
         url: descryptionServiceRoute
     });
     console.log(decryptedData)
-    logger.info(decryptedData.data[0])
-    return decryptedData.data[0]
+    logger.info(decryptedData.data.data[0])
+    return decryptedData.data.data[0]
 }
 
