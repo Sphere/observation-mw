3d9c79310bbf8f0b6dbf4487269a2714a4ae085f
sumit.bajaj-tarento <sumit.bajaj@tarento.com>
2024-01-18 16:39:37 +0530

    nearest percent added

--- src/controllers/observationController.ts
+++ src/controllers/observationController.ts
@@ -213,16 +213,14 @@ export const getObservationSubmissionResult = async (req, res) => {
             method: 'GET',
             url: `${API_ENDPOINTS.dbFind}`,
         })
-        logger.info(submissionResult.data.result)
-        logger.info(submissionResult.data.result[0])
         logger.info(submissionResult.data.result[0].pointsBasedPercentageScore)
 
 
         if (submissionResult) {
             const attemptResultUpdateDetails = {
-                result_percentage: submissionResult.data.result[0].pointsBasedPercentageScore,
-                total_score: submissionResult.data.result[0].pointsBasedMaxScore,
-                acquired_score: submissionResult.data.result[0].pointsBasedScoreAchieved
+                result_percentage: Math.round(submissionResult.data.result[0].pointsBasedPercentageScore),
+                total_score: Math.round(submissionResult.data.result[0].pointsBasedMaxScore),
+                acquired_score: Math.round(submissionResult.data.result[0].pointsBasedScoreAchieved)
             }
             await updateMenteeAttemptDetails(submission_id, attemptResultUpdateDetails)
         }
